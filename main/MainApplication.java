1b433d4996e0c9ef82affa335fd7ae71104ff01d
Abhinav Dhamaniya <abhinavdhamaniya@gmail.com>
2022-11-13 15:56:18 +0530

    Update MainApplication.java

--- main/MainApplication.java
+++ main/MainApplication.java
@@ -1,5 +1,6 @@
 public class MainApplication {
     public static void main(String args[]) {
         System.out.println("Application is running...");
+        System.out.println("This log is added from github...");
     }
 }
